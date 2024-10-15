64ff98a175f08e93cec1057699b100767a24b172
王斌 <bingo084@outlook.com>
2024-10-15 10:39:58 +0800

    feat(updates): refresh updates immediately after update

--- scripts/installupdates.sh
+++ scripts/installupdates.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 refreshUpdates() {
-	ags -r "refreshUpdates()" >/dev/null 2>&1
-	exit
+  ags -r "refreshUpdates()" >/dev/null 2>&1
+  exit
 }
 
-trap refreshUpdates SIGINT SIGTERM EXIT
+trap refreshUpdates SIGINT SIGTERM
 
 ags -r "checkUpdates()" >/dev/null 2>&1
 
@@ -13,5 +13,7 @@ echo "-----------------------------------------------------"
 echo "Start update"
 echo "-----------------------------------------------------"
 paru
+
+ags -r "refreshUpdates()" >/dev/null 2>&1
 
 read -p "Press enter to exit..."
