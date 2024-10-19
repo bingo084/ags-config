39e5831c38c2bcb7900801dfda85235667a891df
王斌 <bingo084@outlook.com>
2024-10-20 00:21:06 +0800

    refactor(weather): remove variable secret and location, fetch directly

--- services/weather.ts
+++ services/weather.ts
@@ -1,5 +1,27 @@
-import { location, secret } from "variables";
+import GLib from "gi://GLib";
 
+interface Secret {
+  weather_key: string;
+  tencent_map: {
+    key: string;
+    secret_key: string;
+  };
+}
+interface Location {
+  ip: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+  ad_info: {
+    nation: string;
+    province: string;
+    city: string;
+    district: string;
+    adcode: number;
+    nation_code: number;
+  };
+}
 interface Weather {
   code: string;
   updateTime: string;
@@ -48,27 +70,43 @@ class WeatherService extends Service {
 
   constructor() {
     super();
-    secret.connect("changed", () => this.sync());
-    location.connect("changed", () => this.sync());
+    this.sync();
     setInterval(() => this.sync(), 10 * 60 * 1000);
   }
 
-  sync() {
-    const { weather_key } = secret.value;
-    const { lat, lng } = location.value.location;
-    if (!weather_key || !lat || !lng) {
-      return;
-    }
+  async sync() {
+    const { weather_key, tencent_map } = this._secret();
+    const location = await this._location(tencent_map);
+    if (!location) return;
+    const { lat, lng } = location.location;
     const url = `https://devapi.qweather.com/v7/weather/now?key=${weather_key}&location=${lng},${lat}`;
-    Utils.fetch(url)
-      .then((res) => res.json())
-      .then((res) => res as Weather)
-      .then((res) => {
-        this.updateProperty("fx_link", res.fxLink);
-        this.updateProperty("now", res.now);
-        this.emit("changed");
-      })
-      .catch((error) => console.log(error));
+    const res = await Utils.fetch(url);
+    const weather = (await res.json()) as Weather;
+    this.updateProperty("fx_link", weather.fxLink);
+    this.updateProperty("now", weather.now);
+    this.emit("changed");
+  }
+
+  private _secret() {
+    const path = GLib.get_user_data_dir() + "/ags/.secret.json";
+    return JSON.parse(Utils.readFile(path) || "{}") as Secret;
+  }
+
+  private async _location({ key, secret_key }: Secret["tencent_map"]) {
+    const url = "https://apis.map.qq.com";
+    const api = `/ws/location/v1/ip?key=${key}`;
+    const sig = Utils.exec([
+      "bash",
+      "-c",
+      `echo -n "${api}${secret_key}" | md5sum | awk '{print $1}'`,
+    ]);
+    try {
+      const res = await Utils.fetch(`${url}${api}&sig=${sig}`);
+      const json = await res.json();
+      return json.result as Location;
+    } catch (e) {
+      console.log(e);
+    }
   }
 }
 
--- variables.ts
+++ variables.ts
@@ -1,5 +1,4 @@
-import GLib from "gi://GLib";
-import Gio from "gi://Gio";
+import weather from "services/weather";
 
 interface Disk {
   size?: string;
@@ -75,63 +74,3 @@ export const updates = Variable(
     ],
   },
 );
-
-interface Secret {
-  weather_key: string;
-  tencent_map: {
-    key: string;
-    secret_key: string;
-  };
-}
-
-const filepath = GLib.get_user_data_dir() + "/ags/.secret.json";
-const readSecret = (file?: Gio.File): Secret =>
-  JSON.parse(Utils.readFile(file ? file : filepath) || "{}");
-Utils.monitorFile(filepath, (file, event) => {
-  if (event === 0) {
-    secret.value = readSecret(file);
-  }
-});
-
-export const secret = Variable(readSecret());
-
-interface Location {
-  ip: string;
-  location: {
-    lat: number;
-    lng: number;
-  };
-  ad_info: {
-    nation: string;
-    province: string;
-    city: string;
-    district: string;
-    adcode: number;
-    nation_code: number;
-  };
-}
-
-const { key, secret_key } = secret.value.tencent_map;
-const url = "https://apis.map.qq.com";
-const api = `/ws/location/v1/ip?key=${key}`;
-const sig = Utils.exec([
-  "bash",
-  "-c",
-  `echo -n "${api}${secret_key}" | md5sum | awk '{print $1}'`,
-]);
-const fetchLocation = () =>
-  Utils.fetch(`${url}${api}&sig=${sig}`)
-    .then((res) => res.json())
-    .then((res) => (location.value = res.result as Location))
-    .catch((error) => console.log(error));
-const network = await Service.import("network");
-let lastConnectivity = network.connectivity;
-network.connect("changed", ({ connectivity }) => {
-  if (connectivity !== lastConnectivity && connectivity === "full") {
-    fetchLocation();
-  }
-  lastConnectivity = connectivity;
-});
-fetchLocation();
-
-export const location = Variable({} as Location);
