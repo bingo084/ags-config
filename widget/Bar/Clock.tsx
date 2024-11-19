740a7897d8c7dacfbe0f6f649c5f8ca9400f759d
王斌 <bingo084@outlook.com>
2024-11-19 14:55:05 +0800

    refactor(clock): simplify time poll

--- widget/Bar/Clock.tsx
+++ widget/Bar/Clock.tsx
@@ -1,31 +1,24 @@
 import Astal from "gi://Astal";
-import { Variable } from "astal";
+import { GLib, Variable } from "astal";
 import { App } from "astal/gtk3";
 
-const date = Variable(new Date()).poll(1000, () => new Date());
-const options = Variable({
-  weekday: "short",
-  month: "short",
-  day: "2-digit",
-  hour: "2-digit",
-  minute: "2-digit",
-  hour12: false,
-} as Intl.DateTimeFormatOptions);
-const dateStr = Variable.derive([date, options], (date, options) =>
-  date.toLocaleString("en-US", options),
+const format = Variable("R");
+const dateTime = Variable<GLib.DateTime | null>(null).poll(1000, () =>
+  GLib.DateTime.new_now_local(),
+);
+const dateTimeStr = Variable.derive(
+  [dateTime, format],
+  (time, format) => time?.format(`%a, %b %d, %${format}`) || "",
 );
 
 const actions = {
   [Astal.MouseButton.PRIMARY]: () => App.toggle_window("calendar"),
   [Astal.MouseButton.MIDDLE]: () =>
-    options.set({
-      ...options.get(),
-      second: options.get().second ? undefined : "2-digit",
-    }),
+    format.set(format.get() === "R" ? "T" : "R"),
 };
 
 export default () => (
   <eventbox onClickRelease={(_, { button }) => actions[button]?.()}>
-    <label label={dateStr()} />
+    <label label={dateTimeStr()} />
   </eventbox>
 );
