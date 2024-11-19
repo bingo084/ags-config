ad6e7242b4b97e1806c6e992d3a3123bb734081f
王斌 <bingo084@outlook.com>
2024-11-20 00:28:03 +0800

    perf(battery): extract same bind

--- widget/Bar/Battery.tsx
+++ widget/Bar/Battery.tsx
@@ -3,22 +3,20 @@ import { bind } from "astal/binding";
 import { Variable } from "astal";
 
 const battery = Battery.get_default();
+const charging = bind(battery, "charging");
+const percentage = bind(battery, "percentage");
 
 const level = (v: number, l1: number, l2: number) =>
   v < l1 ? "critical" : v < l2 ? "warning" : "";
 const className = Variable.derive(
-  [bind(battery, "charging"), bind(battery, "percentage")],
+  [charging, percentage],
   (c, p) => `battery ${c ? "charging" : level(p, 0.2, 0.4)}`,
 );
 
 const formatTime = (second: number) =>
   `${Math.floor(second / 3600)} h ${Math.floor((second % 3600) / 60)} m`;
 const toolTip = Variable.derive(
-  [
-    bind(battery, "timeToEmpty"),
-    bind(battery, "timeToFull"),
-    bind(battery, "charging"),
-  ],
+  [bind(battery, "timeToEmpty"), bind(battery, "timeToFull"), charging],
   (t1, t2, c) => `Time to ${c ? "full" : "empty"}: ${formatTime(c ? t2 : t1)}`,
 );
 
@@ -29,8 +27,6 @@ export default () => (
     tooltipText={toolTip()}
   >
     <icon icon={bind(battery, "batteryIconName")} />
-    <label
-      label={bind(battery, "percentage").as((p) => ` ${(p * 100).toFixed(0)}%`)}
-    />
+    <label label={percentage.as((p) => ` ${(p * 100).toFixed(0)}%`)} />
   </box>
 );
