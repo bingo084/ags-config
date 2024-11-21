25c947d120d19e6900bf46eb55ab7e4088da8bdd
王斌 <bingo084@outlook.com>
2024-11-21 12:05:50 +0800

    feat(workspaces, clients): move full screen indicator to client

--- widget/Bar/Clients.tsx
+++ widget/Bar/Clients.tsx
@@ -1,7 +1,7 @@
 import Apps from "gi://AstalApps";
 import Astal from "gi://Astal";
 import Hyprland from "gi://AstalHyprland";
-import { bind } from "astal/binding";
+import { bind, Variable } from "astal";
 
 const hyprland = Hyprland.get_default();
 const app = new Apps.Apps();
@@ -19,29 +19,36 @@ const actions = {
 const trans = (clazz: string) =>
   clazz === "org.telegram.desktop" ? "telegram" : clazz;
 
+const fc = bind(hyprland, "focusedClient");
+
 export default () => (
-  <box>
+  <box className="clients">
     {bind(hyprland, "clients").as((clients) =>
       clients
         .sort(
           (c1, c2) =>
             c1.workspace.id - c2.workspace.id || c1.x - c2.x || c1.y - c2.y,
         )
-        .map(({ address, class: clazz, title }) => (
-          <button
-            className={bind(hyprland, "focusedClient").as((c) =>
-              c.address === address ? "focused" : "",
-            )}
-            onClickRelease={(_, { button }) => actions[button]?.(address)}
-            tooltipText={title}
-          >
-            <icon
-              icon={
-                app.fuzzy_query(trans(clazz))[0]?.iconName || "image-missing"
-              }
-            />
-          </button>
-        )),
+        .map((client) => {
+          const { address, class: clazz, title } = client;
+          return (
+            <button
+              className={Variable.derive(
+                [fc, bind(client, "fullscreen")],
+                (c, full) =>
+                  `${c.address === address && "focused"} ${full && "fullscreen"}`,
+              )()}
+              onClickRelease={(_, { button }) => actions[button]?.(address)}
+              tooltipText={title}
+            >
+              <icon
+                icon={
+                  app.fuzzy_query(trans(clazz))[0]?.iconName || "image-missing"
+                }
+              />
+            </button>
+          );
+        }),
     )}
   </box>
 );
