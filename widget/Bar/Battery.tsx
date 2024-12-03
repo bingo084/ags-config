85803c4a2e460a009ab699932c4c134c58460f46
王斌 <bingo084@outlook.com>
2024-12-03 10:26:56 +0800

    chore(battery): remove battery class

--- widget/Bar/Battery.tsx
+++ widget/Bar/Battery.tsx
@@ -6,13 +6,6 @@ const battery = Battery.get_default();
 const charging = bind(battery, "charging");
 const percentage = bind(battery, "percentage");
 
-const level = (v: number, l1: number, l2: number) =>
-  v < l1 ? "critical" : v < l2 ? "warning" : "";
-const className = Variable.derive(
-  [charging, percentage],
-  (c, p) => `battery ${c ? "charging" : level(p, 0.2, 0.4)}`,
-);
-
 const formatTime = (second: number) =>
   `${Math.floor(second / 3600)} h ${Math.floor((second % 3600) / 60)} m`;
 const toolTip = Variable.derive(
@@ -21,11 +14,7 @@ const toolTip = Variable.derive(
 );
 
 export default () => (
-  <box
-    className={className()}
-    visible={bind(battery, "isPresent")}
-    tooltipText={toolTip()}
-  >
+  <box visible={bind(battery, "isPresent")} tooltipText={toolTip()}>
     <icon icon={bind(battery, "batteryIconName")} />
     <label label={percentage.as((p) => ` ${(p * 100).toFixed(0)}%`)} />
   </box>
