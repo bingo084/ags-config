5c6a0962e46b1f2e3fc0b628bc4f885b88f46bac
王斌 <bingo084@outlook.com>
2024-12-22 20:24:28 +0800

    fix(systray): use menubutton for gtk4 compatibility

--- widget/Bar/Systray.tsx
+++ widget/Bar/Systray.tsx
@@ -1,7 +1,5 @@
 import Tray from "gi://AstalTray";
-import Astal from "gi://Astal";
 import { bind } from "astal";
-import { App, Gdk } from "astal/gtk3";
 
 const tray = Tray.get_default();
 
@@ -10,30 +8,19 @@ export default () => (
     {bind(tray, "items").as((items) =>
       items
         .filter((item) => item.gicon)
-        .map((item) => {
-          item.iconThemePath && App.add_icons(item.iconThemePath);
-          const menu = item.create_menu();
-          return (
-            <button
-              onClickRelease={(self, { button, x, y }) => {
-                if (button === Astal.MouseButton.PRIMARY) {
-                  item.activate(x, y);
-                } else if (button === Astal.MouseButton.SECONDARY) {
-                  menu?.popup_at_widget(
-                    self,
-                    Gdk.Gravity.SOUTH,
-                    Gdk.Gravity.NORTH,
-                    null,
-                  );
-                }
-              }}
-              onDestroy={() => menu?.destroy()}
-              tooltipMarkup={bind(item, "tooltipMarkup")}
-            >
-              <icon gIcon={bind(item, "gicon")} />
-            </button>
-          );
-        }),
+        .map((item) => (
+          <menubutton
+            tooltipMarkup={bind(item, "tooltipMarkup")}
+            usePopover={false}
+            actionGroup={bind(item, "action-group").as((ag) => [
+              "dbusmenu",
+              ag,
+            ])}
+            menuModel={bind(item, "menu-model")}
+          >
+            <icon gicon={bind(item, "gicon")} />
+          </menubutton>
+        )),
     )}
   </box>
 );
