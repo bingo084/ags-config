f31a9b792d421584ee689200bf8bdd383bbee9ce
王斌 <bingo084@outlook.com>
2025-01-08 11:58:16 +0800

    feat(systray): change primary and secondary click action

--- widget/Bar/Systray.tsx
+++ widget/Bar/Systray.tsx
@@ -1,5 +1,7 @@
 import Tray from "gi://AstalTray";
 import { bind } from "astal";
+import Astal from "gi://Astal";
+import Gdk from "gi://Gdk";
 
 const tray = Tray.get_default();
 
@@ -12,11 +14,20 @@ export default () => (
           <menubutton
             tooltipMarkup={bind(item, "tooltipMarkup")}
             usePopover={false}
-            actionGroup={bind(item, "action-group").as((ag) => [
-              "dbusmenu",
-              ag,
-            ])}
-            menuModel={bind(item, "menu-model")}
+            actionGroup={bind(item, "actionGroup").as((ag) => ["dbusmenu", ag])}
+            menuModel={bind(item, "menuModel")}
+            onButtonReleaseEvent={(self, event) => {
+              const [_, x, y] = event.get_root_coords();
+              const button = event.get_button()[1];
+              const { PRIMARY, SECONDARY } = Astal.MouseButton;
+              const { SOUTH, NORTH } = Gdk.Gravity;
+              if (button === PRIMARY) {
+                item.activate(x, y);
+              } else if (button === SECONDARY) {
+                self.get_popup()?.popup_at_widget(self, SOUTH, NORTH, event);
+              }
+              return true;
+            }}
           >
             <icon gicon={bind(item, "gicon")} />
           </menubutton>
