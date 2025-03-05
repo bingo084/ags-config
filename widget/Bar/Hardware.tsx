b5f6d8a1e046439261359298bd911ba943cf8777
王斌 <bingo084@outlook.com>
2025-03-05 11:15:58 +0800

    refactor(hardware): use GTop instead of script to obtain hardware information

--- widget/Bar/Hardware.tsx
+++ widget/Bar/Hardware.tsx
@@ -1,21 +1,64 @@
 import { GObject, subprocess, Variable } from "astal";
 import { Astal, astalify, ConstructProps, Gtk } from "astal/gtk3";
+import GTop from "gi://GTop";
 
 const level = (v: number, l1: number, l2: number) =>
   v < l1 ? "" : v < l2 ? "warning" : "critical";
 
-const cpu = Variable(0).watch("./script/cpu.sh 2", parseFloat);
-const ram = Variable(0).watch("./script/ram.sh 2", parseFloat);
+const ram = Variable(0).poll(2000, () => {
+  const memory = new GTop.glibtop_mem();
+  GTop.glibtop_get_mem(memory);
+  return (memory.user / memory.total) * 100;
+});
+
+const cpu = Variable.derive(
+  [
+    Variable({ used: 0, total: 0, load: 0 }).poll(
+      2000,
+      ({ used: lastUsed, total: lastTotal }) => {
+        const cpu = new GTop.glibtop_cpu();
+        GTop.glibtop_get_cpu(cpu);
+
+        const used = cpu.user + cpu.sys + cpu.nice + cpu.irq + cpu.softirq;
+        const total = used + cpu.idle + cpu.iowait;
+
+        const diffUsed = used - lastUsed;
+        const diffTotal = total - lastTotal;
+
+        return {
+          used,
+          total,
+          load: diffTotal > 0 ? (diffUsed / diffTotal) * 100 : 0,
+        };
+      },
+    ),
+  ],
+  ({ load }) => load,
+);
+
 const temp = Variable({ cpu: 0, gpu: 0 }).watch("./script/temp.sh 2", (out) =>
   JSON.parse(out),
 );
 const disk = Variable({
   size: "0G",
   used: "0G",
-  avail: "0G",
   use: "0%",
   mount_on: "?",
-}).watch("./script/disk.sh 2", (out) => JSON.parse(out));
+}).poll(2000, () => {
+  const fsusage = new GTop.glibtop_fsusage();
+  GTop.glibtop_get_fsusage(fsusage, "/");
+
+  const size = fsusage.blocks * fsusage.block_size;
+  const used = (fsusage.blocks - fsusage.bfree) * fsusage.block_size;
+  const use = size > 0 ? (used / size) * 100 : 0;
+
+  return {
+    size: `${(size / 1024 ** 3).toFixed(0)}G`,
+    used: `${(used / 1024 ** 3).toFixed(0)}G`,
+    use: `${use.toFixed(0)}%`,
+    mount_on: "/",
+  };
+});
 
 const actions = {
   [Astal.MouseButton.MIDDLE]: () => subprocess("missioncenter"),
@@ -38,7 +81,12 @@ export default () => (
     onClickRelease={(_, { button }) => actions[button]?.()}
     tooltipMarkup={Variable.derive(
       [cpu, ram, temp, disk],
-      (cpu, ram, temp, { used, size, mount_on, use }) => ` CPU : ${cpu}%
+      (
+        cpu,
+        ram,
+        temp,
+        { used, size, mount_on, use },
+      ) => ` CPU : ${cpu.toFixed(1)}%
  RAM : ${ram.toFixed(1)}%
  TEMP: ${temp.cpu}°C(CPU) / ${temp.gpu}°C(GPU)
  DISK: ${used} used out of ${size} on ${mount_on} (${use})`,
--- widget/Bar/Traffic.tsx
+++ widget/Bar/Traffic.tsx
@@ -1,4 +1,5 @@
 import { Variable } from "astal";
+import GTop from "gi://GTop";
 
 const format = (bps: number) => {
   const units = ["B", "KB", "MB", "GB", "TB"];
@@ -19,20 +20,48 @@ const getIcon = (rx: number, tx: number) => {
   return tx > rx ? "network-transmit-symbolic" : "network-receive-symbolic";
 };
 
-const traffic = Variable({
-  icon: "network-idle-symbolic",
-  value: 0,
-  unit: "",
-  visible: false,
-}).watch("./script/traffic.sh 2", (out) => {
-  const { rx_bps, tx_bps } = JSON.parse(out);
-  const maxBps = Math.max(tx_bps, rx_bps);
-  return {
-    icon: getIcon(rx_bps, tx_bps),
-    ...format(maxBps),
-    visible: maxBps >= 102400,
-  };
-});
+const isValidInterface = (interfaceName: string) =>
+  !/^$|lo|veth|tun|tap|docker|br-|meta/.test(interfaceName.toLowerCase());
+
+const traffic = Variable.derive(
+  [
+    Variable({
+      in: 0,
+      out: 0,
+      traffic: {
+        icon: "network-idle-symbolic",
+        value: 0,
+        unit: "",
+        visible: false,
+      },
+    }).poll(2000, ({ in: lastIn, out: lastOut }) => {
+      const netlist = GTop.glibtop_get_netlist(new GTop.glibtop_netlist());
+      let totalIn = 0;
+      let totalOut = 0;
+      netlist.filter(isValidInterface).forEach((_interface) => {
+        const netload = new GTop.glibtop_netload();
+        GTop.glibtop_get_netload(netload, _interface);
+        totalIn += netload.bytes_in;
+        totalOut += netload.bytes_out;
+      });
+
+      const diffIn = lastIn === 0 ? 0 : (totalIn - lastIn) / 2;
+      const diffOut = lastOut === 0 ? 0 : (totalOut - lastOut) / 2;
+      const maxBps = Math.max(diffIn, diffOut);
+
+      return {
+        in: totalIn,
+        out: totalOut,
+        traffic: {
+          icon: getIcon(diffIn, diffOut),
+          ...format(maxBps),
+          visible: maxBps >= 102400,
+        },
+      };
+    }),
+  ],
+  ({ traffic }) => traffic,
+);
 
 export default () => (
   <box className="traffic" visible={traffic(({ visible }) => visible)}>
