f395ec929cef41087e0f5f5378cd63568500c298
王斌 <bingo084@outlook.com>
2025-05-06 08:56:27 +0800

    feat(script): allow restart with inspect

--- script/restart.sh
+++ script/restart.sh
@@ -3,8 +3,17 @@
 gjs_pid=$(pgrep gjs)
 
 if [ -n "$gjs_pid" ]; then
-    pkill -P $gjs_pid
-    kill $gjs_pid
+  pkill -P $gjs_pid
+  kill $gjs_pid
 fi
 
-ags run --gtk4 > ~/.cache/ags/ags.log 2>&1
+ags run --gtk4 >~/.cache/ags/ags.log 2>&1 &
+
+if [[ "$1" == "-i" ]]; then
+  for i in {1..30}; do
+    if ags inspect >/dev/null 2>&1; then
+      break
+    fi
+    sleep 0.1
+  done
+fi
