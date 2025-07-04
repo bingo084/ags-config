3cd9208b34234bd3df33ae4ef1e918c8b0e4c53f
王斌 <bingo084@outlook.com>
2025-07-04 10:29:17 +0800

    refactor(inhibit): replace menubutton with emenubutton, simplify inhibit management

--- widgets/Bar/Inhibit.tsx
+++ widgets/Bar/Inhibit.tsx
@@ -1,45 +1,41 @@
 import { createState } from "ags";
 import { Gtk } from "ags/gtk4";
-import { exec } from "ags/process";
+import app from "ags/gtk4/app";
+import { exec, execAsync, subprocess } from "ags/process";
+import { timeout } from "ags/time";
+import AstalIO from "gi://AstalIO?version=0.1";
 
 interface Inhibit {
-  pid?: number;
+  process?: AstalIO.Process;
   what: string;
 }
 
-const refresh = () => JSON.parse(exec("./scripts/inhibit.sh")) as Inhibit;
-const kill = () => exec(`kill ${inhibit.get().pid}`);
 const create = (what: string) =>
-  exec(
+  subprocess(
     `systemd-inhibit --what=${what} --who=Ags --why='Manual inhibit ${what}' sleep infinity`,
   );
 
-const [inhibit, setInhibit] = createState(refresh());
-
-const actions: Record<number, (widget: Gtk.MenuButton) => void> = {
-  3: (self) => {
-    if (inhibit.get().what === "idle") {
-      kill();
-    } else if (inhibit.get().what === "sleep") {
-      kill();
-      create("idle");
-    } else {
-      create("sleep");
-    }
-    setInhibit(refresh());
-    self.popup();
-  },
-};
+const [inhibit, setInhibit] = createState<Inhibit>({ what: "off" });
+app.connect("shutdown", () => inhibit.get().process?.kill());
 
 export default () => (
-  <menubutton
-  // onButtonReleased={(self, state) => {
-  //   actions[state.get_button()]?.(self);
-  // }}
+  <emenubutton
+    onRightUp={() => {
+      const { what, process } = inhibit.get();
+      if (what === "off") {
+        setInhibit({ what: "sleep", process: create("sleep") });
+      } else if (what === "sleep") {
+        process?.kill();
+        setInhibit({ what: "idle", process: create("idle") });
+      } else {
+        process?.kill();
+        setInhibit({ what: "off" });
+      }
+    }}
   >
     <image iconName="coffee-symbolic" class={inhibit(({ what }) => what)} />
     <popover hasArrow={false}>
       <label label={inhibit(({ what }) => `Inhibit ${what}`)} />
     </popover>
-  </menubutton>
+  </emenubutton>
 );
